5c04123c65f5a66b3a0c7458a32ee6457d7d3d9b
Tyler Hayslett <tyler.m.hayslett@gmail.com>
2021-04-28 00:10:21 -0400

    Added last comments

--- neural_net.c
+++ neural_net.c
@@ -13,10 +13,13 @@ matrix_set * ERRORS;
 matrix_set * WGRADIENT;
 matrix_set * BGRADIENT;
 
-// assuming input will be one single full length column
+// Setup the global variables for a NN
+// Assuming input will be one single full length column
+// Number of total layers must always be >= 1 as the last layer is the output layer
 void setup_network(int input_size, int total_layers, int nodes_per_layer, int output_size){
   int i;
   
+  // Construct all needed matrix sets for network
   WEIGHTS     = new_matrix_set(total_layers);
   BIASES      = new_matrix_set(total_layers);
   Z_SUM       = new_matrix_set(total_layers);
@@ -28,6 +31,7 @@ void setup_network(int input_size, int total_layers, int nodes_per_layer, int ou
   // In new version, activations [0] is just always the image
   ACTIVATIONS->set[0] = new_matrix(input_size,1);
   
+  // each hidden layer is the same size
   int in_width, out_width;
   for(i = 0; i < (total_layers); i++){
     if (i == 0){
@@ -40,6 +44,8 @@ void setup_network(int input_size, int total_layers, int nodes_per_layer, int ou
     } else {
       out_width = nodes_per_layer;
     }
+    
+    // Initialize each array to the correct size
     WEIGHTS->set[i] = rand_matrix(out_width,in_width,1/sqrt(in_width));
     BIASES->set[i] = const_matrix(out_width,1, 1.0);
     Z_SUM->set[i] = new_matrix(out_width,1);
@@ -51,8 +57,8 @@ void setup_network(int input_size, int total_layers, int nodes_per_layer, int ou
   
 }
 
+// Destructor for global network variables, frees everything for next sweep or end of program
 void free_network(){
-  
   free_matrix_set(WEIGHTS);   
   free_matrix_set(BIASES);
   free_matrix_set(Z_SUM);  
@@ -62,6 +68,11 @@ void free_network(){
   free_matrix_set(BGRADIENT);
 }
 
+// Preforms the backprop to find gradient
+// For each layer, you find the error, 
+// The weight gradient is the error times the activations
+// The bias gradient is just the error
+// The gradients are then accumulated
 void back_propogate(matrix * expected){
   int i;
   int num_layers = WEIGHTS->number_of_matrices;
@@ -93,6 +104,8 @@ void back_propogate(matrix * expected){
   }
 }
 
+// Walks through each layer, takes the accumulated gradient from the backpropogations, 
+// and scales it before adding it to the wights and biases
 void train(int num_layers, float training_rate){
   int k;
   
@@ -120,27 +133,34 @@ void train(int num_layers, float training_rate){
 }
 
 
-
+// Walks through each layer, calculating the next layers activations
+// Activations = sigmoid((weights*prior activations) + biases)
 matrix * classify_image(matrix * image){
   int i;
   int num_layers = WEIGHTS->number_of_matrices;
   int temp_row = image->row_cnt;
   int temp_col = image->col_cnt;
   
+  // if not a flat matrix, flatten
   image->row_cnt = image->col_cnt * image->row_cnt;
   image->col_cnt = 1;
   
+  // Set first layer activations to input
   free_matrix(ACTIVATIONS->set[0]);
   ACTIVATIONS->set[0] = scale_matrix(image, 1);
   
+  // Loop through layers
   for(i = 0; i < num_layers; i++){
     free_matrix(Z_SUM->set[i]);
     free_matrix(ACTIVATIONS->set[i+1]);
     
+    // Weight * Activation
     matrix * WA = mult_matrix(WEIGHTS->set[i], ACTIVATIONS->set[i]);
     
+    // Sum of multiplication and biases
     Z_SUM->set[i] = sum_matrix(WA, BIASES->set[i]);
     
+    // Set activations of next layer
     ACTIVATIONS->set[i+1] = sigmoid_matrix(Z_SUM->set[i]);
     //ACTIVATIONS->set[i+1] = ReLU_matrix(Z_SUM->set[i]);
     free_matrix(WA);
@@ -149,11 +169,14 @@ matrix * classify_image(matrix * image){
   image->row_cnt = temp_row;
   image->col_cnt = temp_col;
   
+  // Return classification from output layer
   return scale_matrix(ACTIVATIONS->set[num_layers], 1);
 }  
 
+
 // Lower level Neural Net functions
-// Calculates squared error of output layer in comparison to expected
+// Calculates error of output layer in comparison to expected
+// error = (output - expected) hadamard (derivative of inputs to output layer)
 matrix * calc_output_error(matrix * activations, matrix * required, matrix * Z){
   matrix * Ca = new_matrix(activations->row_cnt, activations->col_cnt);
   matrix * dZ = sig_deriv_matrix(Z);
@@ -172,6 +195,8 @@ matrix * calc_output_error(matrix * activations, matrix * required, matrix * Z){
   return error;
 }
 
+// Calculates error of hidden layer in relation to next layers error
+// error = ((transpose of next layers weights) * (next layers error)) hadamard (derivative of inputs to layer)
 matrix * calc_hidden_error(matrix * prev_error, matrix * prev_weights, matrix * Z){
   matrix * WT = transpose_matrix(prev_weights);
   matrix * product = mult_matrix(WT, prev_error);
